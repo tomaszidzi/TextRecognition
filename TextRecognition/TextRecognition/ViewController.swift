3832dd1f286826a6ee85164d827a5b3ba3c87bfd
Tomasz Idzi <idzitomek@gmail.com>
2020-04-03 17:35:13 +0200

    Connection for UI elements. Implementation of open camera and setup taken picture to preview image view.

--- TextRecognition/TextRecognition/ViewController.swift
+++ TextRecognition/TextRecognition/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Outlets
+    @IBOutlet weak var previewImageView: UIImageView!
+    
+    // MARK: - ViewController life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
 }
 
+// MARK: - Actions
+
+extension ViewController {
+    
+    @IBAction func cameraDidTap(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = UIImagePickerController.isSourceTypeAvailable(.camera) ? .camera : .photoLibrary
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func recognizeDidTap(_ sender: Any) {
+    }
+}
+
+// MARK: - UIImagePickerControllerDelegate
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        
+        guard let image = info[UIImagePickerController.InfoKey(rawValue: UIImagePickerController.InfoKey.originalImage.rawValue)] as? UIImage else { return }
+        previewImageView.image = image
+    }
+}
