77a7ad51fd025e2de1c5e706df627bb17db8fb69
Tomasz Idzi <idzitomek@gmail.com>
2020-04-03 16:48:07 +0200

    Add project to repository and make initial commit.

---
+++ TextRecognition/TextRecognition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextRecognition
+//
+//  Created by Tomasz Idzi on 03/04/2020.
+//  Copyright © 2020 codemeapp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
